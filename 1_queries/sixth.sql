06f9c296dfa9a999cc1a24beafa9350b57d6e1be
AnhLe3698 <anhviet.le00@hotmail.com>
2022-08-19 14:17:39 -0600

    Add Tweet form clear upon success

--- 1_queries/sixth.sql
+++ 1_queries/sixth.sql
@@ -2,3 +2,10 @@ SELECT name, email, phone
 FROM students
 WHERE end_date IS NOT NULL
 AND github IS NULL;
+
+SELECT students.name AS student_name, cohorts.name AS cohort_name,
+cohorts.start_date AS cohort_start_date, students.start_date AS students_start_date
+FROM students
+JOIN cohorts
+ON students.cohort_id = cohorts.id
+WHERE students.start_date <> cohorts.start_date;
