e0acf33be48ebeb7ca377b27dd6278fc62cb17d9
AnhLe3698 <anhviet.le00@hotmail.com>
2022-08-23 15:24:03 -0600

    Add protection against sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,18 +8,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-console.log(args);
+const query = {
+  text: `
+  SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1
+  GROUP BY teachers.name, cohorts.name
+  ORDER BY teacher;
+  `,
+  values: [`%${args[0] || 'JUL02'}%`]
+};
 
-pool.query(`
-SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${args[0] || 'JUL02'}% '
-GROUP BY teachers.name, cohorts.name
-ORDER BY teacher;
-`).then(res => {
+pool.query(query).then(res => {
   res.rows.forEach(user => {
     console.log(`${user.teacher} had contributed ${user.total_assistances} for the ${user.cohort} cohort`);
   });
